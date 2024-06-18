4aabf4758114812f52c0146c596ddb36c9625276
repos-sweetsol <repos.sweetsol@gmail.com>
2024-06-18 16:31:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ from datetime import datetime
 
 
 DB_HOSTNAME = os.environ.get( "DB_HOSTNAME" )
-DB_PORT = os.environ.get( "DB_HOSTNAME" )
-DB_NAME = os.environ.get( "DB_HOSTNAME" )
-DB_USER = os.environ.get( "DB_HOSTNAME" )
-DB_PASSWORD = os.environ.get( "DB_HOSTNAME" )
+DB_PORT = os.environ.get( "DB_PORT" )
+DB_NAME = os.environ.get( "DB_NAME" )
+DB_USER = os.environ.get( "DB_USER" )
+DB_PASSWORD = os.environ.get( "DB_PASSWORD" )
 
 CONN_ARGS = {
     "host": DB_HOSTNAME,
